5bfd5d34e349c36224f5d4301593b228691b6885
Jackson Petty <jacksonopetty@gmail.com>
2024-03-29 10:47:58 -0400

    update scripts

--- run-copy.sh
+++ run-copy.sh
@@ -2,9 +2,9 @@
 
 for j in 1 2 3; do
     for k in 40 64 128 256; do
-        for dmodel in 8 16 32 64 128 256 512; do
+        for dmodel in 8 16 32; do
             for tp in prefix suffix; do
-                sbatch --export=ALL,k=$k,layers=2,batchsize=512,wd=0.01,dmodel=$dmodel,type=$tp launch-copy.slurm
+                sbatch --export=ALL,k=$k,layers=6,batchsize=512,wd=0.01,dmodel=$dmodel,type=$tp launch-copy.slurm
             done
         done 
     done
